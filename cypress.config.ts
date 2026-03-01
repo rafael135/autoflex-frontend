29074d2e0867cee7a0529cf4414c0d85d49f2b78
Rafael Rossi <rtrafaeltrova@gmail.com>
2026-03-01 00:37:31 -0300

    Adds Cypress project ID and removes mock test script

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'nhk3vd',
   e2e: {
     baseUrl: "http://localhost:5173",
     specPattern: "cypress/e2e/**/*.cy.ts",
